5b98f0aa9a964a3696fd79c9e8eb1b3c9453622d
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2020-07-30 14:18:06 +0200

    [IMP] add code button 07xxx

--- spp_custom/__manifest__.py
+++ spp_custom/__manifest__.py
@@ -1,14 +1,15 @@
 # Copyright 2019 Coop IT Easy SCRL fs
 #   Robin Keunen <robin@coopiteasy.be>
+#   Houssine Bakkali <houssine@coopiteasy.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "SPP Customizations",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "depends": ["beesdoo_base", "beesdoo_product"],
     "author": "Coop IT Easy SCRLfs",
     "license": "AGPL-3",
     "category": "",
-    "website": "www.coopiteasy.be",
+    "website": "https://www.coopiteasy.be",
     "summary": """
         Specifics customizations for SPP
     """,
