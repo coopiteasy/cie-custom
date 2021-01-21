9f3081153c2fc4d5547d7a516492b51c656d4bae
robin.keunen <robin@keunen.net>
2021-01-21 17:32:33 +0100

    [REF] foodhub_custom: Black python code

---
+++ foodhub_custom/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2019 Coop IT Easy SCRLfs
+# 	    Robin Keunen <robin@coopiteasy.be>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Foodhub Custom",
+    "version": "12.0.1.0.0",
+    "summary": """
+        Foodhub customizations""",
+    "author": "Coop IT Easy SCRL fs",
+    "website": "http://www.coopiteasy.be",
+    "license": "AGPL-3",
+    "category": "Account",
+    "depends": ["sale_stock", "sale_order_volume"],
+    "data": ["reports/report_deliveryslip.xml", "views/templates.xml"],
+}
---
+++ foodhub_custom/readme/DESCRIPTION.rst
@@ -0,0 +1,6 @@
+
+* compute order volume
+* link pack operation to order line
+* add price information on delivery slip
+* add volume on delivery slip
+* hide product terms from website sale product page
