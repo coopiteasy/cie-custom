c5173fce23367796bc20c46b07637be9646cd489
robin.keunen <robin@keunen.net>
2021-09-02 17:45:43 +0200

    [MIG] provelo_analytic_account: Migration to 12.0

--- provelo_analytic_account/models/account_invoice.py
+++ provelo_analytic_account/models/account_invoice.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 Coop IT Easy SCRL fs
 #   Robin Keunen <robin@coopiteasy.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api
+from odoo import models, fields, api
 
 
 class AccountInvoice(models.Model):
--- provelo_analytic_account/models/sale_order.py
+++ provelo_analytic_account/models/sale_order.py
@@ -2,7 +2,7 @@
 #   Robin Keunen <robin@coopiteasy.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, api
+from odoo import models, api
 
 
 class SaleOrder(models.Model):
