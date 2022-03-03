894b42a04c3a8e44c7b36efe1dc88c4a4e373ecb
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-03-03 14:48:27 +0100

    [ADD] cookingo_custom
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

---
+++ cookingo_custom/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2022 Coop IT Easy SCRLfs
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Cookingo Custom",
+    "summary": """Custom modifications for Cookingo.""",
+    "version": "14.0.1.0.0",
+    "category": "Website",
+    "website": "https://coopiteasy.be",
+    "author": "Coop IT Easy SCRLfs",
+    "license": "AGPL-3",
+    "application": False,
+    "depends": [
+        "website_sale_product_description",
+    ],
+    "excludes": [],
+    "data": [
+        "views/website_sale_template.xml",
+    ],
+    "demo": [],
+    "qweb": [],
+}
