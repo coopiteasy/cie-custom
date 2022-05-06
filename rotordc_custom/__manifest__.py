46bca1d909e031762fc8f6c7d84cac2decd67cad
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-05-06 15:11:50 +0200

    Bump version rotordc_custom
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- rotordc_custom/__manifest__.py
+++ rotordc_custom/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "rotordc_custom",
     "summary": "Customization for RotorDC",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.1.0",
     "category": "Uncategorized",
     "website": "https://coopiteasy.be",
     "author": "Coop IT Easy SCRLfs",
