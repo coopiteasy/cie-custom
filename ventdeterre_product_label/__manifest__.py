64098201529bde48359d03143073fb00b2e779df
Rémy Taymans <remytms@tsmail.eu>
2022-05-10 17:40:31 +0200

    [ADD] vdt_product_label

---
+++ ventdeterre_product_label/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 Coop IT Easy SCRLfs
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Vent de Terre Product Label",
+    "summary": """
+        Adapt default product label for Vent de Terre's needs.""",
+    "version": "11.0.1.0.0",
+    "category": "Sale",
+    "website": "https://coopiteasy.be",
+    "author": "Coop IT Easy SCRLfs",
+    "license": "AGPL-3",
+    "application": False,
+    "depends": ["product"],
+    "excludes": [],
+    "data": ["report/product_product_templates.xml"],
+    "demo": [],
+    "qweb": [],
+}
