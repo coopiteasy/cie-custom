14821e13b5d893f427dd14bd5e5c286e02d13273
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-05-16 15:24:51 +0200

    [ADD] provelo_custom_timesheet_ui
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

---
+++ provelo_custom_timesheet_ui/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2022 Coop IT Easy SCRLfs
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Pro Velo Custom Timesheets UI",
+    "summary": """
+        Small modifications to the Timesheets UI""",
+    "version": "12.0.1.0.0",
+    "category": "Human Resources",
+    "website": "https://coopiteasy.be",
+    "author": "Coop IT Easy SCRLfs",
+    "license": "AGPL-3",
+    "application": False,
+    "depends": [
+        "hr_timesheet_overtime",
+    ],
+    "excludes": [],
+    "data": [
+        "views/hr_timesheet_sheet_views.xml",
+    ],
+    "demo": [],
+    "qweb": [],
+}
