dd7e2ba8b40b4b42c7f23239bb11e00f843f2c39
Victor Champonnois <victor@coopiteasy.be>
2022-07-25 11:19:17 +0200

    [ADD] provelo custom display phone module

---
+++ provelo_custom_display_phone/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2021 Coop IT Easy SCRL fs
+#   Robin Keunen <robin@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Pro Velo Display Phone",
+    "version": "12.0.1.5.0",
+    "depends": [
+        "account",
+        "sale",
+    ],
+    "author": "Coop IT Easy SC",
+    "license": "AGPL-3",
+    "category": "",
+    "website": "https://coopiteasy.be",
+    "summary": "Display phone number in sale order and invoice reports",
+    "data": [
+        "reports/invoice_report.xml",
+        "reports/saleorder_report.xml",
+    ],
+    "installable": True,
+}
---
+++ provelo_custom_display_phone/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Display phone number in invoice and sale order reports. The GSM is used by default, if missing uses the phone.
