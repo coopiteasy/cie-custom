496592323e7f650d5db83e0ee778c91445c2659b
Rémy Taymans <remytms@tsmail.eu>
2022-10-24 17:50:53 +0200

    [ADD] foodhub_custom_product_kanban_view
    [UPD] Update foodhub_custom_product_kanban_view.pot
    
    [UPD] README.rst

---
+++ foodhub_custom_product_kanban_view/__manifest__.py
@@ -0,0 +1,21 @@
+# SPDX-FileCopyrightText: 2022 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+    "name": "Foodhub Custom Product Kanban View",
+    "summary": """
+        Adapt the kanban view for product""",
+    "version": "12.0.1.0.0",
+    "category": "Product",
+    "website": "https://coopiteasy.be",
+    "author": "Coop IT Easy SC",
+    "maintainers": ["remytms"],
+    "license": "AGPL-3",
+    "application": False,
+    "depends": ["product", "stock"],
+    "excludes": [],
+    "data": ["views/product_template_views.xml", "data/stock_product_views.xml"],
+    "demo": [],
+    "qweb": [],
+}
