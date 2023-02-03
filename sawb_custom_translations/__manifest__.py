a9883067fead4372ff369f7510cd218c348ed91f
Victor Champonnois <victor@coopiteasy.be>
2023-02-03 11:35:04 +0100

    [ADD] sawb_custom_translations

---
+++ sawb_custom_translations/__manifest__.py
@@ -0,0 +1,20 @@
+# SPDX-FileCopyrightText: 2023 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+    "name": "SAWB Custom Tranlsations",
+    "summary": """
+        SAWB Custom Tranlsations""",
+    "version": "16.0.1.0.0",
+    "category": "Contact",
+    "website": "https://coopiteasy.be",
+    "author": "Coop IT Easy SC",
+    "maintainers": ["victor-champonnois"],
+    "license": "AGPL-3",
+    "application": False,
+    "depends": ["base"],
+    "excludes": [],
+    "demo": [],
+    "qweb": [],
+}
