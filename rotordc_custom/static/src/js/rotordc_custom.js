f4f3f15e54e52c626c20ea375cea5957d026a385
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2023-03-14 15:55:42 +0100

    [FIX] run pre-commit
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

--- rotordc_custom/static/src/js/rotordc_custom.js
+++ rotordc_custom/static/src/js/rotordc_custom.js
@@ -1,7 +1,7 @@
 // Copyright 2022 Coop IT Easy SC
 // License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-odoo.define("rotordc_custom.rotordc_custom", function (require) {
+odoo.define("rotordc_custom.rotordc_custom", function(require) {
     "use strict";
 
     var sAnimations = require("website.content.snippets.animation");
@@ -9,7 +9,7 @@ odoo.define("rotordc_custom.rotordc_custom", function (require) {
     var WebsiteSale = sAnimations.registry.WebsiteSale;
 
     WebsiteSale.include({
-        _onChangeCombination: function (ev, $parent, combination) {
+        _onChangeCombination: function(ev, $parent, combination) {
             var $barcode = $parent.find(
                 ".oe_product_barcode:first .oe_product_barcode_value"
             );
