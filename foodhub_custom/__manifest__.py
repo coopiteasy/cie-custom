55c8a0ce999c2955ebdd943e97eeef7f7a60a285
Victor Champonnois <victor@coopiteasy.be>
2023-08-09 15:42:20 +0200

    [MIG] foodhub_custom: Migration to 15.0

--- foodhub_custom/__manifest__.py
+++ foodhub_custom/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Foodhub Custom",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "summary": """
         Foodhub customizations""",
     "author": "Coop IT Easy SC",
