77ec2e87e5881f7b821534194797b640b72032a2
hugues de keyzer <odoo@hugues.info>
2023-09-22 15:57:34 +0200

    [REL] foodhub_copy_product_fields 12.0.2.0.0

--- foodhub_copy_product_fields/__manifest__.py
+++ foodhub_copy_product_fields/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Foodhub Copy Product Fields",
     "summary": """Emptied.""",
-    "version": "12.0.1.0.0",
+    "version": "12.0.2.0.0",
     "category": "Uncategorized",
     "website": "https://coopiteasy.be",
     "author": "Coop IT Easy SC",
