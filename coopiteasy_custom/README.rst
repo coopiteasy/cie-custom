63cf94c8614b12f41747c366a70e75c4b435b10b
Robin Keunen <robin@keunen.net>
2024-01-24 14:31:15 +0100

    fixup! [MIG] coopiteasy_custom: Migration to 16.0

--- coopiteasy_custom/README.rst
+++ coopiteasy_custom/README.rst
@@ -17,16 +17,9 @@ Coop IT Easy Customization
     :target: https://github.com/coopiteasy/cie-custom/tree/12.0/coopiteasy_custom
     :alt: coopiteasy/cie-custom
 
-|badge1| |badge2| |badge3| 
+|badge1| |badge2| |badge3|
 
-This module:
-
-* adds reset_so_line on account.analytic.line
-* adds fields on project.task
-* only allows to select open projects in timesheet line views
-* only allows to select tasks of open projects in timesheet line views (when
-  directly selecting a task without selecting a project first)
-* sorts projects by timesheet line count
+This module was emptied on migration to v16. It can be safely uninstalled.
 
 **Table of contents**
 
--- coopiteasy_custom/migrations/pre-migrate.py
+++ coopiteasy_custom/migrations/pre-migrate.py
@@ -1,21 +1,38 @@
-# Copyright 2021 ForgeFlow S.L. (https://www.forgeflow.com)
+# Copyright 2024 Coop IT Easy SC
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openupgradelib import openupgrade
 
-# xmlid_renames = [
-#     (
-#         "coopiteasy_custom.helpesk_ticket_personal_rule",
-#         "helpdesk_mgmt.helpdesk_ticket_personal_rule",
-#     ),
-# ]
+# field definition in v12 of coopiteasy_custom
+#     link_task_ids = fields.Many2many(
+#         comodel_name="project.task",
+#         relation="link_task_relation_table",
+#         column1="user1_id",
+#         column2="user2_id",
+#         string="Linked Tasks",
+
+# field definition in v16 of project
+#     depend_on_ids = fields.Many2many(
+#         "project.task",
+#         relation="task_dependencies_rel",
+#         column1="task_id",
+#         column2="depends_on_id",
+#         string="Blocked By",
+#         tracking=True,
+#         copy=False,
+#         domain="[('project_id', '!=', False), ('id', '!=', id)]",
+#     )
 
 
 @openupgrade.migrate()
 def migrate(env, version):
-    # openupgrade.rename_xmlids(env.cr, xmlid_renames)
-    for task in env["project.task"].search([]):
-        if task.link_task_ids:
-            task.project_id.allow_task_dependencies = True
-            task.write({"depend_on_ids": task.link_task_ids})
+    # there are no hierarchy in linked task field,
+    # I assume col 1 (user1_id) (from here the linked task
+    # are added) is the blocked task
+    env.cr.execute(
+        """
+    insert into task_dependencies_rel
+        select user1_id, user2_id from link_task_relation_table;
+    """
+    )
     env.cr.commit()
