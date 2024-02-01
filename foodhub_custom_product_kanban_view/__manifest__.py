d9028fb0a0851c0db3798613399d259a6875284d
Victor Champonnois <victor@coopiteasy.be>
2024-02-01 11:08:31 +0100

    [MIG] foodhub_custom_product_kanban_view: Migration to 14.0

--- foodhub_custom_product_kanban_view/__manifest__.py
+++ foodhub_custom_product_kanban_view/__manifest__.py
@@ -6,7 +6,7 @@
     "name": "Foodhub Custom Product Kanban View",
     "summary": """
         Adapt the kanban view for product""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Product",
     "website": "https://coopiteasy.be",
     "author": "Coop IT Easy SC",
