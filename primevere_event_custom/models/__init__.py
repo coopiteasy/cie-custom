ba50aa98bfd6220606f7b9421cd4deee1fd24a3f
Rémy Taymans <remytms@tsmail.eu>
2024-10-24 13:02:41 +0200

    [ADD] primevere_e_c: download all tracks images

--- primevere_event_custom/models/__init__.py
+++ primevere_event_custom/models/__init__.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2024 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
 from . import event_track_speaker
 from . import event_track
 from . import event_track_report
