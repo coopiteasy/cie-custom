d8b229a772c356ef8e06db115ab49d49796a9893
hugues de keyzer <odoo@hugues.info>
2024-11-11 19:11:15 +0100

    [MIG] rotordc_custom: migration to 14.0

--- rotordc_custom/__manifest__.py
+++ rotordc_custom/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "rotordc_custom",
     "summary": "Customization for RotorDC",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Uncategorized",
     "website": "https://coopiteasy.be",
     "author": "Coop IT Easy SC",
