6934b44f398b2c1966bece4dc8a983caeba571f0
hugues de keyzer <odoo@hugues.info>
2024-12-06 10:39:18 +0100

    [MIG] rotordc_report_picking_huge_sale_order: migration to 13.0

--- rotordc_report_picking_huge_sale_order/__manifest__.py
+++ rotordc_report_picking_huge_sale_order/__manifest__.py
@@ -3,22 +3,19 @@
 
 {
     "name": "RotorDC Stock Picking Operations Report Huge Sale Order Text",
-    "summary": """
-        At the bottom of the Picking Operations report, display the sale order in huge
-        text.""",
-    "version": "12.0.1.0.0",
-    "category": "Warehouse",
+    "summary": (
+        "At the bottom of the Picking Operations report, display the sale "
+        "order in huge text"
+    ),
+    "version": "13.0.1.0.0",
+    "category": "Operations/Inventory",
     "website": "https://coopiteasy.be",
     "author": "Coop IT Easy SC",
     "license": "AGPL-3",
-    "application": False,
     "depends": [
         "stock",
     ],
-    "excludes": [],
     "data": [
         "report/report_stockpicking_operations.xml",
     ],
-    "demo": [],
-    "qweb": [],
 }
