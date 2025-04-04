67c6379a2dd6a65b4c7c7727bf4c6d6e6a83bcb3
hugues de keyzer <odoo@hugues.info>
2025-04-04 18:44:46 +0200

    [ADD] new module lpcr_pos

---
+++ lpcr_pos/readme/DESCRIPTION.rst
@@ -0,0 +1,19 @@
+Customize the PoS according to LPCR specifications.
+
+This module hides several buttons:
+
+* Top header:
+
+  * Cash In/Out
+  * Orders
+* Product screen:
+
+  * Refund
+  * Info
+* Customer selection screen:
+
+  * Create
+  * Details
+* Payment screen:
+
+  * Customer
---
+++ lpcr_pos/static/src/js/Screens/ProductScreen/ProductScreen.esm.js
@@ -0,0 +1,20 @@
+/** @odoo-module **/
+// SPDX-FileCopyrightText: 2025 Coop IT Easy SC
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import ProductScreen from "point_of_sale.ProductScreen";
+import Registries from "point_of_sale.Registries";
+
+const LPCRProductScreen = (ProductScreen_) =>
+    class extends ProductScreen_ {
+        get controlButtons() {
+            return super.controlButtons.filter(
+                (button) => !["RefundButton", "ProductInfoButton"].includes(button.name)
+            );
+        }
+    };
+
+Registries.Component.extend(ProductScreen, LPCRProductScreen);
+
+export default ProductScreen;
