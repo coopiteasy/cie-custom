01b44575809753040e45a9d2a7bd8045a91b52db
Simon Hick <sim@coopiteasy.be>
2025-06-03 17:07:48 +0200

    [ADD] Add a button redirecting to the webshop

---
+++ bees_custom_webshop_button/__manifest__.py
@@ -0,0 +1,24 @@
+# SPDX-FileCopyrightText: 2025 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+    "name": "Bees Custom Webshop Button",
+    "summary": "Add a button in Bees' website redirecting to the webshop",
+    "version": "12.0.1.0.0",
+    "category": "Uncategorized",
+    "website": "https://coopiteasy.be",
+    "author": "Coop IT Easy SC",
+    "maintainers": ["mihien"],
+    "license": "AGPL-3",
+    "depends": [
+        "beesdoo_website_theme",
+        "cooperator",
+        "partner_firstname",
+        "website",
+    ],
+    "data": [
+        "views/assets.xml",
+        "views/website_templates.xml",
+    ],
+}
