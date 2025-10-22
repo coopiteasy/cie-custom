f68b73c0a4cecbefb2880187cb361b566ce02aa0
Rémy Taymans <remytms@tsmail.eu>
2025-10-22 10:52:29 +0200

    [LIN] ugess_membership_interview: remove unecessary string of fields

--- ugess_membership_interview/models/res_partner.py
+++ ugess_membership_interview/models/res_partner.py
@@ -4,8 +4,8 @@ from odoo import fields, models
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    entry_interview_done = fields.Boolean(string="Entry Interview Done")
-    exit_interview_done = fields.Boolean(string="Exit Interview Done")
+    entry_interview_done = fields.Boolean()
+    exit_interview_done = fields.Boolean()
 
     referent_id = fields.Many2one(
         comodel_name="res.partner",
