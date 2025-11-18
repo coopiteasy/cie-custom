0b476181fdc8aa4ae05e0fe6f1db677a3d303cbc
Julien Klindt <julien@whitebalance.de>
2025-11-18 21:05:20 +0100

    add custom overrides for scb

---
+++ scb_custom/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "SUPERCOOP Bremen Website Overrides",
+    "summary": "Navbar-, User-Menü- und Become-Cooperator-Anpassungen für SUPERCOOP Bremen",
+    "version": "16.0.1.0.0",
+    "author": "SUPERCOOP Bremen eG",
+    "website": "https://supercoop-bremen.de",
+    "category": "Website",
+    "license": "LGPL-3",
+    "depends": [
+        "website",
+        "portal",
+        "cooperator_website",
+    ],
+    "data": [
+        "views/website_navbar.xml",
+        "views/website_user_menu.xml",
+        "views/website_become_cooperator.xml",
+    ],
+    "assets": {},
+    "installable": True,
+    "application": False,
+}
