3bc2beec5f3d68d19e4c1567a649e688e2cc9b88
Simon Hick <sim@coopiteasy.be>
2026-04-10 10:53:19 +0200

    [ADD] lpcr_custom_membership_expiry_mail

---
+++ lpcr_custom_membership_expiry_mail/__manifest__.py
@@ -0,0 +1,23 @@
+# SPDX-FileCopyrightText: 2026 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+    "name": "LPCR Custom Membership Expiry Mail",
+    "summary": "Send a mail to members whose membership will expire in a month.",
+    "version": "16.0.1.0.0",
+    "category": "Association",
+    "website": "https://coopiteasy.be",
+    "author": "Coop IT Easy SC, Odoo Community Association (OCA)",
+    "maintainers": ["mihien"],
+    "license": "AGPL-3",
+    "depends": [
+        "mail",
+        "membership",
+        "social_media",
+    ],
+    "data": [
+        "data/ir_cron.xml",
+        "data/membership_expiry_mail.xml",
+    ],
+}
