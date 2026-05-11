1ac97ae4554b002a9c08fbed1b1b045f824a1da3
Simon Hick <sim@coopiteasy.be>
2026-05-11 11:28:21 +0200

    [ADD] lpcr_custom_restricted_product_access

---
+++ lpcr_custom_restricted_product_access/__manifest__.py
@@ -0,0 +1,20 @@
+# SPDX-FileCopyrightText: 2026 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+    "name": "LPCR Custom Restricted product Access",
+    "summary": "Restrict write, create and delete access for products to a single group",
+    "version": "16.0.1.0.0",
+    "category": "Uncategorized",
+    "website": "https://coopiteasy.be",
+    "author": "Coop IT Easy SC",
+    "maintainers": ["mihien"],
+    "license": "AGPL-3",
+    "depends": [
+        "product",
+    ],
+    "data": [
+        "security/security.xml",
+    ],
+}
---
+++ lpcr_custom_restricted_product_access/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+Restrict write, create and delete access for products to a single group.
+
+Mind that if any other group rule grant one of these accesses back to users, the restriction for that access will be lifted. 
