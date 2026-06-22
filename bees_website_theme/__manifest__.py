c6e870bcef6b114b84a8704d09a67e69ba70e45c
Rémy Taymans <remytms@tsmail.eu>
2026-06-22 16:13:08 +0200

    [MIG] beesdoo_website_theme: Migration to 16.0
    This module is renamed to bees_website_theme.

--- bees_website_theme/__manifest__.py
+++ bees_website_theme/__manifest__.py
@@ -4,11 +4,14 @@
         Apply BEES coop design rules.
     """,
     "author": "Rémy Taymans, Coop IT Easy SC",
-    "website": "https://github.com/beescoop/Obeesdoo",
+    "website": "https://coopiteasy.be",
     "license": "AGPL-3",
     "category": "Themes",
-    "version": "12.0.0.0.1",
-    "application": True,
+    "version": "16.0.0.0.1",
     "depends": ["website"],
-    "data": ["views/assets.xml"],
+    "assets": {
+        "web.assets_frontend": [
+            "bees_website_theme/static/src/less/bees_website_design.less",
+        ],
+    },
 }
---
+++ bees_website_theme/readme/CONTRIBUTORS.rst
@@ -0,0 +1,7 @@
+* `Coop IT Easy SC <https://coopiteasy.be>`_:
+
+  * Rémy Taymans
+
+* `BEES coop - Cellule IT`:
+
+  * Rémy Taymans
