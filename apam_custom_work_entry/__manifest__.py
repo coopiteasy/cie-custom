dc8eca6b1dfaaa70fe05a951edc99639e018ace1
hugues de keyzer <odoo@hugues.info>
2026-07-21 17:49:33 +0200

    [ADD] add new module apam_custom_work_entry

---
+++ apam_custom_work_entry/__manifest__.py
@@ -0,0 +1,20 @@
+# SPDX-FileCopyrightText: 2026 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+    "name": "APAM Custom Work Entry",
+    "summary": "Work Entries customization for APAM",
+    "version": "18.0.1.0.0",
+    "category": "Human Resources/Employees",
+    "website": "https://github.com/coopiteasy/cie-custom",
+    "author": "Coop IT Easy SC",
+    "maintainers": ["huguesdk"],
+    "license": "AGPL-3",
+    "depends": [
+        "hr_work_entry",
+    ],
+    "data": [
+        "views/hr_work_entry_view.xml",
+    ],
+}
---
+++ apam_custom_work_entry/readme/DESCRIPTION.md
@@ -0,0 +1,5 @@
+Work Entries customization for APAM.
+
+This modules adds:
+
+*   A comment field on work entries (editable on the form)
